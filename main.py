98705f2f217eaaf14f8dbce5e14cad5f63625faa
mabruk <mabruk.dev@gmail.com>
2025-02-02 22:23:39 +0530

    asteroid player collision

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -35,6 +36,10 @@ def main():
         for d in drawable:
             d.draw(screen)
         updatable.update(dt)
+        for a in asteroids:
+            if a.check_collision_with(player):
+                print("Game over! ")
+                sys.exit()
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
